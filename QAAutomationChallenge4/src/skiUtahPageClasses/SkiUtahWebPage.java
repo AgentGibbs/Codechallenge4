d4d0771a90debefaa1956722e4d0cc211e3b877e
AgentGibbs <christian.gibbs@stgconsulting.com>
2016-08-02 16:06:36 -0600

    This should have all the necessary files to build in Maven and run the TestNG tests.

---
+++ QAAutomationChallenge4/src/skiUtahPageClasses/SkiUtahWebPage.java
@@ -0,0 +1,80 @@
+package skiUtahPageClasses;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import supportUtils.WebDriverHelper;
+
+public abstract class SkiUtahWebPage {
+	private String pageName;
+	private String pageURL;
+	private String expectedURL;
+	private WebDriver browserDriver;
+	public List<WebElement> mainMenuWebElements;
+	public List<WebElement> subMenuWebElements;
+	private WebDriverHelper helper;
+
+
+	public SkiUtahWebPage()
+	{
+		helper = new WebDriverHelper();
+	}
+	
+	public SkiUtahWebPage(WebDriver driver)
+	{
+		helper = new WebDriverHelper();
+		setDriver(driver);
+	}
+	
+	public List<WebElement> pageElements;
+
+	public String getExpectedURL() {
+		return expectedURL;
+	}
+
+	public void setExpectedURL(String expectedURL) {
+		this.expectedURL = expectedURL;
+	}
+
+	public String getPageTitle() {
+		return browserDriver.getTitle();
+	}
+
+	
+
+	public String getPageName() {
+		return pageName;
+	}
+
+	public void setPageName(String pageName) {
+		this.pageName = pageName;
+	}
+
+	public String getPageURL() {
+		return pageURL;
+	}
+
+	public void setPageURL(String pageURL) {
+		this.pageURL = pageURL;
+	}
+
+	public void clickElement(WebElement clickableControl) {
+
+	}
+
+	public void navigateToDefault()
+	{
+		browserDriver.get(getPageURL());
+		helper.Wait(5000);
+	}
+	public WebDriver getDriver() {
+		return browserDriver;
+	}
+
+	public void setDriver(WebDriver driver) {
+		this.browserDriver = driver;
+	}
+	
+}
